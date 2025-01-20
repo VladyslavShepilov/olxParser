aafce4ea12eac9d412c00ec29c7218132e89b2c4
vladsepilov <crm.33379@gmail.com>
2025-01-21 00:22:38 +0200

    add validation

--- olx/items.py
+++ olx/items.py
@@ -1,12 +1,36 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+from dataclasses import dataclass, field
+from typing import Optional
+from datetime import datetime
+import locale
 
-import scrapy
+@dataclass
+class OlxItem:
+    id: int
+    views: int
+    title: str
+    price: str
+    published_at: str
+    description: str = field(default=None)
+    images: Optional[str] = field(default=None)
+    tags: Optional[str] = field(default=None)
 
+    def __post_init__(self):
+        try:
+            self.published_at = parse_ukrainian_date(self.published_at)
+        except ValueError:
+            raise ValueError(f"Invalid publication date format: {self.published_at}")
+        
 
-class OlxItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+def parse_ukrainian_date(date_str):
+    try:
+        locale.setlocale(locale.LC_TIME, "uk_UA.UTF-8")
+        
+        if "Сьогодні" in date_str:
+            return datetime.now()
+        
+        cleaned_date = date_str.replace(" р.", "").strip()
+        date_obj = datetime.strptime(cleaned_date, "%d %B %Y")
+        return date_obj
+    
+    except (ValueError, locale.Error):
+        raise ValueError(f"Invalid date string: {date_str}")
