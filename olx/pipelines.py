ce13a0bc4d127a4810286861784e1cc5c8947f39
vladsepilov <crm.33379@gmail.com>
2025-01-21 16:10:38 +0200

    configure pipeline

--- olx/pipelines.py
+++ olx/pipelines.py
@@ -5,9 +5,22 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from config.database import get_session
+from olx.items import OlxItem
 
+class DatabasePipeline:
+    def __init__(self):
+        self.session = get_session()
 
-class OlxPipeline:
     def process_item(self, item, spider):
+        existing_item = self.session.query(OlxItem).filter(OlxItem.url == item['olx_od']).first()
+        
+        if not existing_item:
+            # Add new item if no duplicates
+            new_item = OlxItem(**item)
+            self.session.add(new_item)
+            self.session.commit()
         return item
+
+    def close_spider(self, spider):
+        self.session.close()
