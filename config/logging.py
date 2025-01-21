31bc7be27155061f83459b3cd55ef7942ea82826
vladsepilov <crm.33379@gmail.com>
2025-01-21 16:11:24 +0200

    reconfigure logging

--- config/logging.py
+++ config/logging.py
@@ -1,9 +1,22 @@
-import logging
 from loguru import logger
+import logging
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            level = logger.level(record.levelname).name
+        except KeyError:
+            level = record.levelno
+
+        logger.log(level, record.getMessage())
 
 def setup_logging():
-    logger.remove()  # Remove default logger
-    logger.add("logs/{time}.log", rotation="1 day", retention="7 days", compression="zip")
-    logging.basicConfig(level=logging.INFO)
-    logger.info("Logging setup completed.")
-
+    logger.remove()  # Remove default Loguru handler
+    logger.add(
+        "logs/{time:YYYY-MM-DD}.log",
+        rotation="1 day",
+        retention="7 days",
+        compression="zip",
+        level="INFO",
+    )
+    logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
