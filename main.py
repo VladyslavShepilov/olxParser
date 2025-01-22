faa5b36cfd2bc6a5f74217786c8c49a4e531fc84
vladsepilov <crm.33379@gmail.com>
2025-01-22 17:01:18 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from config.logging import setup_logging
-from config.database import create_tables
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+
+from config.database import create_tables
+from config.logging import setup_logging
 from olx.spiders.olx_spider import OlxSpider
+
 
 def main():
     setup_logging()
     create_tables()
 
+    # TODO - add to dockerfile in loop cycle (the easiest)
     process = CrawlerProcess(get_project_settings())
     process.crawl(OlxSpider)
 
     process.start()
+
 
 if __name__ == "__main__":
     main()
