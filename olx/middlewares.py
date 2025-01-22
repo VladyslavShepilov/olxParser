d3aa16bfcaad0242d4657ae40f93c9c883b8ee3d
vladsepilov <crm.33379@gmail.com>
2025-01-22 16:53:04 +0200

    settings adjustments

--- olx/middlewares.py
+++ olx/middlewares.py
@@ -1,13 +1,14 @@
+import time
+
 from scrapy.http import HtmlResponse
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class OlxDownloaderMiddleware:
@@ -18,17 +19,20 @@ class OlxDownloaderMiddleware:
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("start-maximized")
-        options.add_argument("--disable-gpu") 
+        options.add_argument("--disable-gpu")
 
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+        self.driver = webdriver.Chrome(
+            service=Service(ChromeDriverManager().install()), options=options
+        )
 
     def process_request(self, request, spider):
         spider.logger.info(f"Loading content of: {request.url}")
         self.driver.get(request.url)
-        
+
         if not request.url.startswith("https://www.olx.ua/uk/list/?page="):
             self.scroll_once()
+            time.sleep(3)
+            
             WebDriverWait(self.driver, 10).until(
                 lambda d: d.execute_script("return document.readyState") == "complete"
             )
@@ -36,8 +40,9 @@ class OlxDownloaderMiddleware:
             self.scroll_to_bottom()
 
         page_source = self.driver.page_source
-        return HtmlResponse(url=request.url, body=page_source, encoding="utf-8", request=request)
-    
+        return HtmlResponse(
+            url=request.url, body=page_source, encoding="utf-8", request=request
+        )
 
     def scroll_once(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
--- olx/settings.py
+++ olx/settings.py
@@ -6,6 +6,10 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from config.logging import setup_logging
+
+setup_logging()
+
 
 BOT_NAME = "olx"
 
@@ -13,12 +17,15 @@ SPIDER_MODULES = ["olx.spiders"]
 NEWSPIDER_MODULE = "olx.spiders"
 
 
+RETRY_ENABLED = True
+RETRY_TIMES = 5
+RETRY_HTTP_CODES = [500, 502, 503, 504, 408, 403, 401]
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 )
-
 
 
 # Obey robots.txt rules
@@ -32,14 +39,14 @@ CONCURRENT_REQUESTS = 16
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
@@ -55,36 +62,37 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "olx.pipelines.OlxPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "olx.pipelines.OlxItemValidationPipeline": 100,
+    "olx.pipelines.OlxDatabasePipeline": 200,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = "httpcache"
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
