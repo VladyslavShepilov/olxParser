9f80ba2d532c0a599ca029eeeac82d8c5be68c32
vladsepilov <crm.33379@gmail.com>
2025-01-22 16:55:31 +0200

    pipeline refactor

--- olx/pipelines.py
+++ olx/pipelines.py
@@ -1,26 +1,50 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from loguru import logger
+from pydantic import ValidationError
+from scrapy.exceptions import DropItem
 
-
-# useful for handling different item types with a single interface
 from config.database import get_session
-from olx.items import OlxItem
+from models.olx_item import OlxItem
+from olx.items import ItemValidator
 
-class DatabasePipeline:
+
+class OlxDatabasePipeline:
     def __init__(self):
         self.session = get_session()
+        self.logger = logger
+        self.batch_size = 10
 
     def process_item(self, item, spider):
-        existing_item = self.session.query(OlxItem).filter(OlxItem.url == item['olx_id']).first()
-        
+        self.logger.warning(f"Processing item: {item['olx_id']}")
+        existing_item = (
+            self.session.query(OlxItem).filter(OlxItem.olx_id == item["olx_id"]).first()
+        )
+
         if not existing_item:
-            # Add new item if no duplicates
             new_item = OlxItem(**item)
             self.session.add(new_item)
+
+        if len(self.session.new) >= self.batch_size:
             self.session.commit()
+            self.logger.warning(f"Committing batch of {self.batch_size} items.")
+
         return item
 
     def close_spider(self, spider):
+        if self.session.new:
+            self.session.commit()
         self.session.close()
+
+        def close_spider(self, spider):
+            self.logger.warning("Closing the spider and closing the database session.")
+            self.session.close()
+
+
+class OlxItemValidationPipeline:
+    def process_item(self, item, spider):
+        try:
+            validated_item = ItemValidator(**item)
+            logger.warning(f"Validated item: {validated_item}")
+            return validated_item.model_dump()
+        except ValidationError as e:
+            logger.error(f"Validation error for item {item.get('olx_id')}: {e}")
+            raise DropItem(f"Invalid item: {e}") from e
